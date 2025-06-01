b1337bd22391e53ae8d778bff679eb410cfb7a12
Joel Lima <joel@marsaude.net>
2025-06-01 02:29:01 -0300

    fix: environment prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   token: 'f9b698a1d0330b731f960cc18771115e3c2d18ef54980214cac440d472324313abdb41b2da99fd80170e7deb76e93cf735559a2233cefe72d8724fbf2bc8ba72e3c4b4b62e3f15f7ee23d22767870f93ed3aeee1ecf740f0f73da30f518d4990ab4ee519c4fecd17426d0f046c7864e79f329e3897d14297a3f1a6ea6299fdff',
-  apiUrl: 'http://localhost:1337/api',
-  loginRedirect: 'http://localhost:3200',
+  apiUrl: 'https://cop30placesadmin.onrender.com/api',
+  loginRedirect: 'https://belregiamap.netlify.app',
 };
